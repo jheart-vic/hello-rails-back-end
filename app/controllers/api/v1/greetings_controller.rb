e63d1ad53eb622833692fbb72348d06258cca8d9
jheart-vic <adebowalevictorjheart@gmail.com>
2022-10-19 00:05:17 +0100

    Fix linters

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::GreetingsController < ApplicationController
   def index
     greetings = Greeting.all.sample.content
     if greetings
-      render json: { status: 'SUCCESS', message: 'Fetched all the greetings successfully', data: greetings }, status: :ok
+      render json: { status: 'SUCCESS', message: 'Fetched all the greetings successfully', data: greetings },
+             status: :ok
     else
       render json: greetings.errors, status: :bad_request
     end
